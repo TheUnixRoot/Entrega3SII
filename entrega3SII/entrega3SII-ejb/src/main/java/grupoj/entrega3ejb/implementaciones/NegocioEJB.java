a700df094355ed51395a487fd2df261a53d2fdf4
Juan Pedro Dominguez Berdun <juanpedrodb.96@gmail.com>
2017-05-21 15:56:36 +0200

    Creado el EJB y su interfaz, pom con nueva depend

---
+++ entrega3SII/entrega3SII-ejb/src/main/java/grupoj/entrega3ejb/implementaciones/NegocioEJB.java
@@ -0,0 +1,409 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupoj.entrega3ejb.implementaciones;
+
+import grupoj.entrega3ejb.interfaces.PersistenceMock;
+import grupoj.prentrega1.Administrador;
+import grupoj.prentrega1.Anuncio;
+import grupoj.prentrega1.Evento;
+import grupoj.prentrega1.Formulario;
+import grupoj.prentrega1.Geolocalizacion;
+import grupoj.prentrega1.Lugar;
+import grupoj.prentrega1.Mensaje;
+import grupoj.prentrega1.Notificacion;
+import grupoj.prentrega1.Periodista;
+import grupoj.prentrega1.Tag;
+import grupoj.prentrega1.Usuario;
+import grupoj.prentrega1.Valoracion_eve;
+import grupoj.prentrega1.Valoracion_lug;
+import java.io.Serializable;
+import java.util.List;
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author juanp
+ */
+@Local(PersistenceMock.class)
+@Stateless
+public class NegocioEJB implements Serializable, PersistenceMock {
+    
+    @PersistenceContext(unitName = "grupoj_prEntrega1_jar_1.0-SNAPSHOTPU")
+    private EntityManager em;
+    
+    public NegocioEJB() {
+        
+    }
+
+    @Override
+    public List<Usuario> getListaUsuarios() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        TypedQuery<Usuario> q = em.createQuery("select u from Usuario u", Usuario.class);
+        return q.getResultList();
+    }
+
+    @Override
+    public void setListaUsuarios(List<Usuario> listaUsuarios) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        listaUsuarios.forEach(em::merge);
+    }
+
+    @Override
+    public List<Periodista> getListaPeriodistas() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        TypedQuery<Periodista> q = em.createQuery("select u from Periodista u", Periodista.class);
+        return q.getResultList();
+    }
+
+    @Override
+    public void setListaPeriodistas(List<Periodista> listaPeriodistas) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        listaPeriodistas.forEach(em::merge);
+    }
+
+    @Override
+    public List<Administrador> getListaAdministradores() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        TypedQuery<Administrador> q = em.createQuery("select u from Administrador u", Administrador.class);
+        return q.getResultList();
+    }
+
+    @Override
+    public void setListaAdministradores(List<Administrador> listaAdministradores) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        listaAdministradores.forEach(em::merge);
+    }
+
+    @Override
+    public List<Evento> getListaEventos() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        TypedQuery<Evento> q = em.createQuery("select u from Evento u", Evento.class);
+        return q.getResultList();
+    }
+
+    @Override
+    public void setListaEventos(List<Evento> listaEventos) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        listaEventos.forEach(em::merge);
+    }
+
+    @Override
+    public List<Lugar> getListaLugares() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        TypedQuery<Lugar> q = em.createQuery("select u from Lugar u", Lugar.class);
+        return q.getResultList();
+    }
+
+    @Override
+    public void setListaLugares(List<Lugar> listaLugares) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        listaLugares.forEach(em::merge);
+    }
+
+    @Override
+    public List<Tag> getListaTags() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        TypedQuery<Tag> q = em.createQuery("select u from Tag u", Tag.class);
+        return q.getResultList();
+    }
+
+    @Override
+    public void setListaTags(List<Tag> listaTags) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        listaTags.forEach(em::merge);
+    }
+
+    @Override
+    public List<Anuncio> getListaAnuncios() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        TypedQuery<Anuncio> q = em.createQuery("select u from Anuncio u", Anuncio.class);
+        return q.getResultList();
+    }
+
+    @Override
+    public void setListaAnuncios(List<Anuncio> listaAnuncios) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        listaAnuncios.forEach(em::merge);
+    }
+
+    @Override
+    public List<Mensaje> getListaMensajes() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        TypedQuery<Mensaje> q = em.createQuery("select u from Mensaje u", Mensaje.class);
+        return q.getResultList();
+    }
+
+    @Override
+    public void setListaMensajes(List<Mensaje> listaMensajes) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        listaMensajes.forEach(em::merge);
+    }
+
+    @Override
+    public List<Valoracion_eve> getListaValoracion_eves() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        TypedQuery<Valoracion_eve> q = em.createQuery("select u from Valoracion_eve u", Valoracion_eve.class);
+        return q.getResultList();
+    }
+
+    @Override
+    public void setListaValoracion_eves(List<Valoracion_eve> listaValoracion_eves) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        listaValoracion_eves.forEach(em::merge);
+    }
+
+    @Override
+    public List<Valoracion_lug> getListaValoracion_lugs() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        TypedQuery<Valoracion_lug> q = em.createQuery("select u from Valoracion_lug u", Valoracion_lug.class);
+        return q.getResultList();
+    }
+
+    @Override
+    public void setListaValoracion_lugs(List<Valoracion_lug> listaValoracion_lugs) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        listaValoracion_lugs.forEach(em::merge);
+    }
+
+    @Override
+    public List<Notificacion> getListaNotificaciones() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        TypedQuery<Notificacion> q = em.createQuery("select u from Notificacion u", Notificacion.class);
+        return q.getResultList();
+    }
+
+    @Override
+    public void setListaNotificaciones(List<Notificacion> listaNotificaciones) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        listaNotificaciones.forEach(em::merge);
+    }
+
+    @Override
+    public List<Geolocalizacion> getListaGeolocalizaciones() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        TypedQuery<Geolocalizacion> q = em.createQuery("select u from Geolocalizacion u", Geolocalizacion.class);
+        return q.getResultList();
+    }
+
+    @Override
+    public void setListaGeolocalizaciones(List<Geolocalizacion> listaGeolocalizaciones) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        listaGeolocalizaciones.forEach(em::merge);
+    }
+
+    @Override
+    public List<Formulario> getListaFormularios() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        TypedQuery<Formulario> q = em.createQuery("select u from Formulario u", Formulario.class);
+        return q.getResultList();
+    }
+
+    @Override
+    public void setListaFormularios(List<Formulario> listaFormularios) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        listaFormularios.forEach(em::merge);
+    }
+
+    @Override
+    public void setUsuario(Usuario usr) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(getListaUsuarios().contains(usr))
+            em.merge(usr);
+        else
+            em.persist(usr);
+    }
+
+    @Override
+    public Usuario getUsuario(Long id) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return em.find(Usuario.class, id);
+    }
+
+    @Override
+    public void setPeriodista(Periodista per) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(getListaPeriodistas().contains(per))
+            em.merge(per);
+        else
+            em.persist(per);
+    }
+
+    @Override
+    public Periodista getPeriodista(Long id) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return em.find(Periodista.class, id);
+    }
+
+    @Override
+    public void setAdministrador(Administrador adm) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(getListaAdministradores().contains(adm))
+            em.merge(adm);
+        else
+            em.persist(adm);
+    }
+
+    @Override
+    public Administrador getAdministrador(Long id) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return em.find(Administrador.class, id);
+    }
+
+    @Override
+    public void setEvento(Evento eve) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(getListaEventos().contains(eve))
+            em.merge(eve);
+        else
+            em.persist(eve);
+    }
+
+    @Override
+    public Evento getEvento(Long id) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return em.find(Evento.class, id);
+    }
+
+    @Override
+    public void setLugar(Lugar lug) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(getListaLugares().contains(lug))
+            em.merge(lug);
+        else
+            em.persist(lug);
+    }
+
+    @Override
+    public Lugar getLugar(Long id) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return em.find(Lugar.class, id);
+    }
+
+    @Override
+    public void setTag(Tag tg) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(getListaTags().contains(tg))
+            em.merge(tg);
+        else
+            em.persist(tg);
+    }
+
+    @Override
+    public Tag getTag(Long id) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return em.find(Tag.class, id);
+    }
+
+    @Override
+    public void setValoracion_eve(Valoracion_eve veve) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(getListaValoracion_eves().contains(veve))
+            em.merge(veve);
+        else
+            em.persist(veve);
+    }
+
+    @Override
+    public Valoracion_eve getValoracion_eve(Long id) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return em.find(Valoracion_eve.class, id);
+    }
+
+    @Override
+    public void setValoracion_lug(Valoracion_lug vlug) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(getListaValoracion_lugs().contains(vlug))
+            em.merge(vlug);
+        else
+            em.persist(vlug);
+    }
+
+    @Override
+    public Valoracion_lug getValoracion_lug(Long id) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return em.find(Valoracion_lug.class, id);
+    }
+
+    @Override
+    public void setMensaje(Mensaje msg) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(getListaMensajes().contains(msg))
+            em.merge(msg);
+        else
+            em.persist(msg);
+    }
+
+    @Override
+    public Mensaje getMensaje(Long id) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return em.find(Mensaje.class, id);
+    }
+
+    @Override
+    public void setNotificacion(Notificacion ntf) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(getListaNotificaciones().contains(ntf))
+            em.merge(ntf);
+        else
+            em.persist(ntf);
+    }
+
+    @Override
+    public Notificacion getNotificacion(Long id) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return em.find(Notificacion.class, id);
+    }
+
+    @Override
+    public void setGeolocaclizacion(Geolocalizacion geo) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(getListaGeolocalizaciones().contains(geo))
+            em.merge(geo);
+        else
+            em.persist(geo);
+    }
+
+    @Override
+    public Geolocalizacion getGeolocalizacion(Long id) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return em.find(Geolocalizacion.class, id);
+    }
+
+    @Override
+    public void setFormulario(Formulario frm) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(getListaFormularios().contains(frm))
+            em.merge(frm);
+        else
+            em.persist(frm);
+    }
+
+    @Override
+    public Formulario getFormulario(Long id) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return em.find(Formulario.class, id);
+    }
+
+    @Override
+    public void setAnuncio(Anuncio adv) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(getListaAnuncios().contains(adv))
+            em.merge(adv);
+        else
+            em.persist(adv);
+    }
+
+    @Override
+    public Anuncio getAnuncio(Long id) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return em.find(Anuncio.class, id);
+    }
+
+}
---
+++ entrega3SII/entrega3SII-ejb/src/main/java/grupoj/entrega3ejb/interfaces/PersistenceMock.java
@@ -0,0 +1,277 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupoj.entrega3ejb.interfaces;
+
+import grupoj.prentrega1.Administrador;
+import grupoj.prentrega1.Anuncio;
+import grupoj.prentrega1.Evento;
+import grupoj.prentrega1.Formulario;
+import grupoj.prentrega1.Geolocalizacion;
+import grupoj.prentrega1.Lugar;
+import grupoj.prentrega1.Mensaje;
+import grupoj.prentrega1.Notificacion;
+import grupoj.prentrega1.Periodista;
+import grupoj.prentrega1.Tag;
+import grupoj.prentrega1.Usuario;
+import grupoj.prentrega1.Valoracion_eve;
+import grupoj.prentrega1.Valoracion_lug;
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author juanp
+ */
+@Local
+public interface PersistenceMock {
+
+    public List<Usuario> getListaUsuarios();
+
+    public void setListaUsuarios(List<Usuario> listaUsuarios);
+
+    public List<Periodista> getListaPeriodistas();
+
+    public void setListaPeriodistas(List<Periodista> listaPeriodistas);
+
+    public List<Administrador> getListaAdministradores();
+
+    public void setListaAdministradores(List<Administrador> listaAdministradores);
+
+    public List<Evento> getListaEventos();
+
+    public void setListaEventos(List<Evento> listaEventos);
+
+    public List<Lugar> getListaLugares();
+
+    public void setListaLugares(List<Lugar> listaLugares);
+
+    public List<Tag> getListaTags();
+
+    public void setListaTags(List<Tag> listaTags);
+
+    public List<Anuncio> getListaAnuncios();
+
+    public void setListaAnuncios(List<Anuncio> listaAnuncios);
+
+    public List<Mensaje> getListaMensajes();
+
+    public void setListaMensajes(List<Mensaje> listaMensajes);
+
+    public List<Valoracion_eve> getListaValoracion_eves();
+
+    public void setListaValoracion_eves(List<Valoracion_eve> listaValoracion_eves);
+
+    public List<Valoracion_lug> getListaValoracion_lugs();
+
+    public void setListaValoracion_lugs(List<Valoracion_lug> listaValoracion_lugs);
+
+    public List<Notificacion> getListaNotificaciones();
+
+    public void setListaNotificaciones(List<Notificacion> listaNotificaciones);
+
+    public List<Geolocalizacion> getListaGeolocalizaciones();
+
+    public void setListaGeolocalizaciones(List<Geolocalizacion> listaGeolocalizaciones);
+
+    public List<Formulario> getListaFormularios();
+
+    public void setListaFormularios(List<Formulario> listaFormularios);
+
+    /**
+     * Inserta o actualiza un usuario en el contexto de persistencia.
+     *
+     * @param usr Entidad Usuario en cuestion
+     */
+    public void setUsuario(Usuario usr);
+
+    /**
+     * Devuelve del contexto de persistencia el usuario pedido
+     *
+     * @param id Id del Usuario requerido
+     * @return Usuario solicitado o null si no existiese
+     */
+    public Usuario getUsuario(Long id);
+
+    /**
+     * Inserta o actualiza un periodista en el contexto de persistencia.
+     *
+     * @param per Entidad Periodista en cuestion
+     */
+    public void setPeriodista(Periodista per);
+
+    /**
+     * Devuelve del contexto de persistencia el periodista pedido
+     *
+     * @param id Id del periodista requerido
+     * @return periodista solicitado o null si no existiese
+     */
+    public Periodista getPeriodista(Long id);
+
+    /**
+     * Inserta o actualiza un administrador en el contexto de persistencia.
+     *
+     * @param adm Entidad Administrador en cuestion
+     */
+    public void setAdministrador(Administrador adm);
+
+    /**
+     * Devuelve del contexto de persistencia el administrador pedido
+     *
+     * @param id Id del administrador requerido
+     * @return administrador solicitado o null si no existiese
+     */
+    public Administrador getAdministrador(Long id);
+
+    /**
+     * Inserta o actualiza un evento en el contexto de persistencia.
+     *
+     * @param eve Entidad Evento en cuestion
+     */
+    public void setEvento(Evento eve);
+
+    /**
+     * Devuelve del contexto de persistencia el evento pedido
+     *
+     * @param id Id del Evento requerido
+     * @return Evento solicitado o null si no existiese
+     */
+    public Evento getEvento(Long id);
+
+    /**
+     * Inserta o actualiza un lugar en el contexto de persistencia.
+     *
+     * @param lug Entidad Lugar en cuestion
+     */
+    public void setLugar(Lugar lug);
+
+    /**
+     * Devuelve del contexto de persistencia el lugar pedido
+     *
+     * @param id Id del Lugar requerido
+     * @return Lugar solicitado o null si no existiese
+     */
+    public Lugar getLugar(Long id);
+
+    /**
+     * Inserta o actualiza un tag en el contexto de persistencia.
+     *
+     * @param tg Entidad Tag en cuestion
+     */
+    public void setTag(Tag tg);
+
+    /**
+     * Devuelve del contexto de persistencia el tag pedido
+     *
+     * @param id Id del Tag requerido
+     * @return Tag solicitado o null si no existiese
+     */
+    public Tag getTag(Long id);
+
+    /**
+     * Inserta o actualiza una valoracion_eve en el contexto de persistencia.
+     *
+     * @param veve Entidad Valoracion_eve en cuestion
+     */
+    public void setValoracion_eve(Valoracion_eve veve);
+
+    /**
+     * Devuelve del contexto de persistencia la Valoracion_eve pedido
+     *
+     * @param id Id de la Valoracion_eve requerido
+     * @return Valoracion_eve solicitada o null si no existiese
+     */
+    public Valoracion_eve getValoracion_eve(Long id);
+
+    /**
+     * Inserta o actualiza una valoracion_lug en el contexto de persistencia.
+     *
+     * @param vlug Entidad Valoracion_lug en cuestion
+     */
+    public void setValoracion_lug(Valoracion_lug vlug);
+
+    /**
+     * Devuelve del contexto de persistencia la Valoracion_lug pedido
+     *
+     * @param id Id de la Valoracion_lug requerido
+     * @return Valoracion_lug solicitada o null si no existiese
+     */
+    public Valoracion_lug getValoracion_lug(Long id);
+
+    /**
+     * Inserta o actualiza un mensaje en el contexto de persistencia.
+     *
+     * @param msg Entidad Mensaje en cuestion
+     */
+    public void setMensaje(Mensaje msg);
+
+    /**
+     * Devuelve del contexto de persistencia el mensaje pedido
+     *
+     * @param id Id del mensaje requerido
+     * @return mensaje solicitado o null si no existiese
+     */
+    public Mensaje getMensaje(Long id);
+
+    /**
+     * Inserta o actualiza una notificacion en el contexto de persistencia.
+     *
+     * @param ntf Entidad Notificacion en cuestion
+     */
+    public void setNotificacion(Notificacion ntf);
+
+    /**
+     * Devuelve del contexto de persistencia la notificacion pedida
+     *
+     * @param id Id de la notificacion requerida
+     * @return notificacion solicitada o null si no existiese
+     */
+    public Notificacion getNotificacion(Long id);
+
+    /**
+     * Inserta o actualiza una geolocalizacion en el contexto de persistencia.
+     *
+     * @param geo Entidad Geolocalizacion en cuestion
+     */
+    public void setGeolocaclizacion(Geolocalizacion geo);
+
+    /**
+     * Devuelve del contexto de persistencia la geolocalizacion pedida
+     *
+     * @param id Id de la geolocalizacion requerida
+     * @return geolocalizacion solicitada o null si no existiese
+     */
+    public Geolocalizacion getGeolocalizacion(Long id);
+
+    /**
+     * Inserta o actualiza un formulario en el contexto de persistencia.
+     *
+     * @param frm Entidad Formulario en cuestion
+     */
+    public void setFormulario(Formulario frm);
+
+    /**
+     * Devuelve del contexto de persistencia el formulario pedido
+     *
+     * @param id Id del formulario requerido
+     * @return formulario solicitado o null si no existiese
+     */
+    public Formulario getFormulario(Long id);
+
+    /**
+     * Inserta o actualiza un anuncio en el contexto de persistencia.
+     *
+     * @param adv Entidad Anuncio en cuestion
+     */
+    public void setAnuncio(Anuncio adv);
+
+    /**
+     * Devuelve del contexto de persistencia el anuncio pedido
+     *
+     * @param id Id del anuncio requerido
+     * @return anuncio solicitado o null si no existiese
+     */
+    public Anuncio getAnuncio(Long id);
+}
